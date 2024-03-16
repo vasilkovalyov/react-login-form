242c41b29e907012d370ca2430e2a512d3fc39e9
vasilkovalyov <vasilkovalyov@gmail.com>
2024-03-16 13:12:26 +0100

    update auth layout

--- src/layouts/auth/auth.tsx
+++ src/layouts/auth/auth.tsx
@@ -12,8 +12,8 @@ function AuthLayout({ children, title, className }: AuthContainerProps) {
   return (
     <section className={cn('auth', className)}>
       <div className="auth__container container">
-        <img src="/logo.svg" alt="qencode" />
-        <h2 className="auth-title">{title}</h2>
+        <img src="/logo.svg" alt="qencode" className="auth__logo" />
+        <h2 className="auth__title">{title}</h2>
         <div className="auth__content">{children}</div>
       </div>
     </section>
