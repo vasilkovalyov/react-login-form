9b6b41d61ed6b8eb584de487275a6caac02162da
vasilkovalyov <vasilkovalyov@gmail.com>
2024-03-16 12:01:14 +0100

    create auth layout component

---
+++ src/layouts/auth/auth.tsx
@@ -0,0 +1,23 @@
+import cn from 'classnames';
+
+import './auth.scss';
+
+type AuthContainerProps = {
+  title?: string;
+  children?: React.ReactNode;
+  className?: string;
+};
+
+function AuthLayout({ children, title, className }: AuthContainerProps) {
+  return (
+    <section className={cn('auth', className)}>
+      <div className="auth__container container">
+        <img src="/logo.svg" alt="qencode" />
+        <h2 className="auth-title">{title}</h2>
+        <div className="auth__content">{children}</div>
+      </div>
+    </section>
+  );
+}
+
+export default AuthLayout;
